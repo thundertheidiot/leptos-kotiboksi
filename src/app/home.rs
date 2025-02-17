e067a3346a34cac3eb765a165a9f4655348f91f2
Thunder <thunder@disroot.org>
2025-02-17 20:54:36 +0200

    update homepage

--- src/app/home.rs
+++ src/app/home.rs
@@ -6,71 +6,114 @@ pub fn HomePage() -> impl IntoView {
     view! {
         <div>
             <div class=ClassName::TEXTPART>
-                <h1>"About me:"</h1>
+            <h1>"Hi"</h1>
+
+            <p>
+            "I'm thunder, welcome to my website "
+            </p>
+
+        <h2>"About"</h2>
+
+        <p>
+        "I like computers and tinkering with them, i don't have much else going on in my life."
+        </p>
 
                 <p>
-                    "I like tinkering with computers, they are my only real hobby. "
-                    "I've used (GNU/)Linux since 2017, I currently use "
+                    "I've been using (GNU/)Linux since 2017, i currently use "
                     <a href="https://nixos.org">"NixOS"</a>
-                    ", which is a pretty niche distro that allows you to declaratively configure pretty much everything. "
-                    <a href="https://gitlab.com/thundertheidiot/nixdots">"Here"</a>
+                    ", which is a pretty niche distro that allows you to declaratively configure pretty much everything using nix. "
+            <a href="https://github.com/thundertheidiot/nixdots">"Here"</a>
+
                     " is my slowly growing monstrocity of a config."
                 </p>
 
-                <h2>"Programming:"</h2>
+            <p>
+            "My favourite language for general purpose programming is Rust. "
+            "I have a basic understanding of C and C++, but I'm not a huge fan. "
+            </p>
 
-                <p>
-                    "Currently my favourite language for general purpose programming is Rust. "
-                    "I have a basic understanding of C and C++, but I'm not a huge fan of them. "
-                    "I also like trying \"weird\" languages like Haskell or "
-                    <a href="https://janet-lang.org">"Janet"</a>
-                    ", but I haven't really used them outside of small utilities. "
-                    "Working with Nix (the language) in the context of a configuration is quite nice."
-                </p>
+            <p>
+            "I also like trying more obscure languages languages like Haskell or Common Lisp"
+            ", but i haven't really used them outside of small utility programs, some of which can be found embedded in my nix configuration. "
+            "I also like Nix (the language)."
+            </p>
 
             </div>
 
-            <br/>
+            <br />
 
             <div class=ClassName::TEXTPART>
-                <h1>"This website:"</h1>
+                <h2>"This website"</h2>
 
                 <p>
-                    "The website is made with the "
+                    "This site was made with the "
                     <a href="https://leptos-rs.github.io/leptos/">"leptos framework"</a>
-                    ", because I wanted to make something with Rust. "
-                    "There isn't really anything here that i couldn't make with "
-                    <em>"insert javascript framework"</em> ", i just like working with Rust."
+            ", because i wanted to program in Rust. "
+        "All of this could easily be created with basic html, a sprinkle of js for the radio and a tiny bit of php for the guestbook, "
+        "in fact that is what i used previously."
                 </p>
 
-                <p>
-                    "There aren't any real downsides to using leptos either. The WASM blob is very small in size compared to websites of today, as of writing it is under 400kB, not even accounting for compression (to give some perspective, the front page of google currently weighs 3.52 MB). Additionally even if you don't have WASM enabled for whatever reason the page will still mostly work thanks to server side rendering."
-                </p>
+        <p>
+            "Thanks to leptos' server side rendering feature, there aren't really any downsides to using it here, "
+        "even with WASM disabled most of the page (everything \"important\") works. "
+        "The WASM blob is very small compared to other modern websites."
+        </p>
 
                 <p>
-                    " The font is IBM VGA 8x16, and you can find it "
+                    "The pixel font is IBM VGA 8x16, and you can find it "
                     <a href="https://int10h.org/oldschool-pc-fonts/">"here"</a> "."
                 </p>
+
             </div>
 
-            <br/>
+            <br />
 
             <div class=ClassName::TEXTPART>
-                <h1>"Other stuff:"</h1>
+            <h2>"Misc"</h2>
 
-                <p>
-                    "I am running a " <a href="https://chat.kotiboksi.xyz">"matrix"</a>
-                    " and xmpp server on here in addition to this website. Feel free to sign up to either, registrations are open."
-                </p>
+        <ul>
+        <li><a href="https://github.com/thundertheidiot">"github"</a></li>
+        <li><a href="https://gitlab.com/thundertheidiot">"gitlab"</a></li>
+        <li><a href="https://codeberg.org/thundertheidiot">"codeberg"</a>" (not much on here yet)"</li>
+            </ul>
 
-                <h2>"Contact:"</h2>
+            <img src="/static/img/buttons/gnu-linux.gif" alt="gnu/linux" />
+            " "
+            <img src="/static/img/buttons/emacs.gif" alt="emacs" />
+            " "
+            <img src="/static/img/buttons/pride.gif" alt="pridenow" />
+            " "
+            <img src="/static/img/buttons/transnow2.gif" alt="transnow" />
+            " "
+
+        <br/>
+
+    // when published
+        // <img src="/static/img/buttons/agplv3-88x31.gif" alt="agplv3" />
+        // " "
+
+            <a href="https://cyber.dabamos.de/88x31"><img src="/static/img/buttons/88x31.gif" alt="the buttons" /></a>
+            " "
+            <a href="https://nixos.org"><img src="/static/img/buttons/nixos.png" alt="nixos" /></a>
+            " "
+            <a href="https://maia.crimew.gay"><img src="/static/img/buttons/maia.png" alt="maia.crimew.gay" /></a>
+            " "
+            <a href="https://cartercoding.com"><img src="/static/img/buttons/carter.webp" alt="carer i am carer" /></a>
+
+
+            </div>
+
+        <br/>
+
+        <div class=ClassName::TEXTPART>
+                <h2>"Contact"</h2>
 
                 <p style="margin:0px">"You can find me on"</p>
                 <p>
-                    "XMPP: " <a>"thunder@gooptyland.xyz"</a> <br/> "Matrix: "
-                    <a>"@thunder:kotiboksi.xyz"</a> <br/> "Discord: " <a>"thundertheidiot"</a>
+                    "XMPP: " <a>"thunder@gooptyland.xyz"</a> <br /> "Matrix: "
+                    <a>"@thunder:kotiboksi.xyz"</a> <br /> "Discord: " <a>"thundertheidiot"</a>
                 </p>
-            </div>
+        </div>
         </div>
     }
 }
